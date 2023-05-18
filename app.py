f7a01c3bbafeead6ddd057a6e9a58be86e5ff775
avivlyweb <73378473+avivlyweb@users.noreply.github.com>
2023-05-18 12:18:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -53,15 +53,16 @@ def fetch_pubmed(article_ids):
     articles_data = soup.find_all("PubmedArticle")
     return articles_data
 
-# Extract MeSH terms and abstract from the articles data
-def get_mesh_terms(articles_data):
+# Extract MeSH terms, abstract, and study type from the articles data
+def get_mesh_terms_and_study_type(articles_data):
     articles = []
     for article_data in articles_data:
         article_id = article_data.find("PMID").text
         url = f"https://pubmed.ncbi.nlm.nih.gov/{article_id}"
         mesh_terms = [mesh_term.text for mesh_term in article_data.find_all("DescriptorName")]
         abstract = article_data.find("AbstractText").text if article_data.find("AbstractText") else ""
-        articles.append({"id": article_id, "url": url, "mesh_terms": mesh_terms, "abstract": abstract})
+        publication_type = [publication_type.text for publication_type in article_data.find_all("PublicationType")]
+        articles.append({"id": article_id, "url": url, "mesh_terms": mesh_terms, "abstract": abstract, "publication_type": publication_type})
     return articles
 
 # Get user input
@@ -74,11 +75,11 @@ if st.button("Search with EBPcharlie"):
     else:
         article_ids = search_pubmed(user_input)
         articles_data = fetch_pubmed(article_ids)
-        articles = get_mesh_terms(articles_data)
+        articles = get_mesh_terms_and_study_type(articles_data)
         st.write(f"Found {len(articles)} articles related to your clinical question.")
 
-        # Generate a list of PMIDs, URLs and MeSH terms
-        article_list = "\n".join([f"PMID: {article['id']} URL: {article['url']} MeSH Terms: {', '.join(article['mesh_terms'])}" for article in articles])
+        # Generate a list of PMIDs, URLs, MeSH terms, and study types
+        article_list = "\n".join([f"PMID: {article['id']} URL: {article['url']} MeSH Terms: {', '.join(article['mesh_terms'])} Study Type: {', '.join(article['publication_type'])}" for article in articles])
 
         # Generate prompt for OpenAI API
         prompt = f"Using your expert knowledge, analyze the following systematic reviews related to '{user_input}' published between 2019-2023:\n{article_list}\n\nPlease provide a structured analysis with the following sections:\n\n1. Summary of Findings:\n- Provide a brief summary of the main findings of these articles.\n\n2. Important Outcomes (with PMID, URL, and MeSH terms):\n- List the most important outcomes in bullet points and ensure that the PMID, URL, and MeSH terms mentioned for each outcome correspond to the correct article.\n\n3. Comparisons and Contrasts:\n- Highlight any key differences or similarities between the findings of these articles.\n\n4. Innovative Treatments or Methodologies:\n- Are there any innovative treatments or methodologies mentioned in these articles that could have significant impact on the field?\n\n5. Future Research and Unanswered Questions:\n- Briefly discuss any potential future research directions or unanswered questions based on the findings of these articles.\n\n6. Conclusion:\n- Sum up the main takeaways from these articles."
@@ -88,11 +89,12 @@ if st.button("Search with EBPcharlie"):
         st.subheader("Summary of Findings")
         st.write(summary)
 
-        # Display article abstracts and MeSH terms
-        st.subheader("Article Abstracts and MeSH terms")
+        # Display article abstracts, MeSH terms, and study types
+        st.subheader("Article Abstracts, MeSH terms, and Study Types")
         for article in articles:
             st.write(f"PMID: {article['id']}")
             st.write(f"URL: {article['url']}")
             st.write(f"MeSH terms: {', '.join(article['mesh_terms'])}")
+            st.write(f"Study type: {', '.join(article['publication_type'])}")
             st.write(article["abstract"])
-            st.write("\n\n\n") 
+            st.write("\n\n\n")
