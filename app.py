cca15bee41727a1cb5148316767022dd82ae05ed
avivlyweb <73378473+avivlyweb@users.noreply.github.com>
2023-05-18 12:43:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import json
 import streamlit as st
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import html2text
 
 # Set up OpenAI API credentials
 openai.api_key = st.secrets["OPENAI_API_KEY"]
@@ -20,12 +19,26 @@ params = {
     "api_key": "5cd7903972b3a715e29b76f1a15001ce9a08"
 }
 
+# App Heading and Description
+st.markdown(
+    """
+    # EBPCharlie: An AI-Powered Literature Review Assistant
+
+    EBPCharlie utilizes AI technology to help automate and accelerate the literature review process for researchers. 
+    By integrating PubMed API and OpenAI's GPT-3, EBPCharlie can search for scientific articles based on a given 
+    PICO (Patient/Problem, Intervention, Comparison, Outcome) framework, extract relevant information from those articles,
+    and provide a structured summary of the findings.
+    
+    Simply input your PICO criteria, and EBPCharlie will do the rest!
+    """
+)
+
 # Define function to generate text using OpenAI API
 def generate_text(prompt):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=2024,
+        max_tokens=1000,
         n=1,
         stop=None,
         temperature=0.7,
@@ -64,19 +77,18 @@ def get_mesh_terms(articles_data):
         articles.append({"id": article_id, "url": url, "mesh_terms": mesh_terms, "abstract": abstract})
     return articles
 
-# Get user input for PICO
-p_input = st.text_input("Patient, Population, or Problem")
-i_input = st.text_input("Intervention")
-c_input = st.text_input("Comparison")
-o_input = st.text_input("Outcome")
-
-user_input = f"{p_input} {i_input} {c_input} {o_input}"
+# Get user PICO input
+patient = st.text_input("Patient, Population, or Problem")
+intervention = st.text_input("Intervention")
+comparison = st.text_input("Comparison")
+outcome = st.text_input("Outcome")
 
 # Search for articles using Pubmed API
 if st.button("Search with EBPcharlie"):
-    if not user_input.strip():
-        st.error("Please enter the PICO components to search for articles.")
+    if not patient or not intervention or not outcome:
+        st.error("Please enter Patient, Intervention, and Outcome to search for articles.")
     else:
+        user_input = f"{patient} AND {intervention} AND {outcome} AND {comparison}"
         article_ids = search_pubmed(user_input)
         articles_data = fetch_pubmed(article_ids)
         articles = get_mesh_terms(articles_data)
@@ -86,7 +98,7 @@ if st.button("Search with EBPcharlie"):
         article_list = "\n".join([f"PMID: {article['id']} URL: {article['url']} MeSH Terms: {', '.join(article['mesh_terms'])}" for article in articles])
 
         # Generate prompt for OpenAI API
-        prompt = f"Using your expert knowledge, analyze the following systematic reviews related to '{user_input}' published between 2019-2023:\n{article_list}\n\nPlease provide a structured analysis with the following sections:\n\n1. Summary of Findings:\n- Provide a brief summary of the main findings of these articles.\n\n2. Important Outcomes (with PMID, URL, and MeSH terms):\n- List the most important outcomes in bullet points and ensure that the PMID, URL, and MeSH terms mentioned for each outcome correspond to the correct article.\n\n3. Comparisons and Contrasts:\n- Highlight any key differences or similarities between the findings of these articles.\n\n4. Innovative Treatments or Methodologies:\n- Are there any innovative treatments or methodologies mentioned in these articles that could have significant impact on the field?\n\n5. Future Research and Unanswered Questions:\n- Briefly discuss any potential future research directions or unanswered questions based on the findings of these articles.\n\n6. Conclusion:\n- Sum up the main takeaways from these articles."
+        prompt = f"Using your expert knowledge, analyze the following articles related to the PICO question '{user_input}' published recently:\n{article_list}\n\nPlease provide a structured analysis with the following sections:\n\n1. Summary of Findings:\n- Provide a brief summary of the main findings of these articles.\n\n2. Important Outcomes (with PMID, URL, and MeSH terms):\n- List the most important outcomes in bullet points and ensure that the PMID, URL, and MeSH terms mentioned for each outcome correspond to the correct article.\n\n3. Comparisons and Contrasts:\n- Highlight any key differences or similarities between the findings of these articles.\n\n4. Innovative Treatments or Methodologies:\n- Are there any innovative treatments or methodologies mentioned in these articles that could have significant impact on the field?\n\n5. Future Research and Unanswered Questions:\n- Briefly discuss any potential future research directions or unanswered questions based on the findings of these articles.\n\n6. Conclusion:\n- Sum up the main takeaways from these articles."
 
         # Generate summary using OpenAI API
         summary = generate_text(prompt)
@@ -100,4 +112,4 @@ if st.button("Search with EBPcharlie"):
             st.write(f"URL: {article['url']}")
             st.write(f"MeSH terms: {', '.join(article['mesh_terms'])}")
             st.write(article["abstract"])
-            st.write("\n\n\n") 
+            st.write("\n\n\n")
